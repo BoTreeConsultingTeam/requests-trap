a0e4b67e9b02fab96c48d31c6de5a0aa4f8e2393
Jignesh Gohel <jignesh.gohel@botreeconsulting.com>
2014-08-27 13:02:10 +0530

    Requests Trap Rails application basic skeleton.
    Extra Additions:
      1. Removed public/index.html
      2. Added entry in .gitignore to exclude config/database.yml to be committed to remote repository
      3. Added a database.yml template file named "database.yml.postgresql.template" (for PostgreSQL) with placeholders to facilitate developers to use it with minimal changes.
      4. Updated Gemfile by adding following gems:
    
           gem 'pg'  (for using the PostgreSQL as the DB)
           gem "twitter-bootstrap-rails" (for using Twitter Bootstrap Framework for Frontend themeing)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RequestsTrap::Application.config.secret_token = '562e80bd489ebd0f8b883ff4f7c3083a7ebc6bf2d99876c10dce2a12a43b9e39c63a5f16cdf5f8410a38e3f68ad525bd50993220aa5b81871e1e1e787ba2cf0b'
